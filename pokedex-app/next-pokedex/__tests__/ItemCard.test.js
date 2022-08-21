a529fd5ed65d870731227415023a0cca283808b7
Paul Csapak <paul@base34.com>
2022-08-21 22:26:52 +0200

    next-pokedex: add unit tests and jest snapshot tests

---
+++ pokedex-app/next-pokedex/__tests__/ItemCard.test.js
@@ -0,0 +1,27 @@
+import { render, screen } from '@testing-library/react'
+import ItemCard from '../components/ItemCard'
+
+const ITEM_DATA = {
+  postId: 1,
+  name: 'Bulbasaur',
+  height: 100,
+  weight: 100,
+  types: [{ type: { name: 'grass' } }],
+}
+
+describe('ItemCard', () => {
+  it('renders a heading', () => {
+    render(<ItemCard {...ITEM_DATA} />)
+
+    const heading = screen.getByRole('heading', {
+      name: `#${ITEM_DATA.postId}: ${ITEM_DATA.name}`,
+    })
+
+    expect(heading).toBeInTheDocument()
+  })
+
+  it('renders correctly', () => {
+    const { container } = render(<ItemCard {...ITEM_DATA} />)
+    expect(container).toMatchSnapshot()
+  })
+})
