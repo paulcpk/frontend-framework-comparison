bd8f8f018e00d0b276307d4889ae12b3c686f849
Paul Csapak <paul@base34.com>
2022-08-31 09:43:07 +0400

    Re-Init nuxt app without Buefy

--- pokedex-app/nuxt-pokedex/nuxt.config.js
+++ pokedex-app/nuxt-pokedex/nuxt.config.js
@@ -21,7 +21,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['bulma'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -39,8 +39,6 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/buefy
-    'nuxt-buefy',
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
   ],
