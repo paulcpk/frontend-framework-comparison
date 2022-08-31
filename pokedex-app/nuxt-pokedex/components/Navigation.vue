fa0b1339e8c8af12f41418e767409e5fb13a5c6b
Paul Csapak <paul@base34.com>
2022-08-31 10:02:28 +0400

    nuxt-pokedex: basic template and component setup

---
+++ pokedex-app/nuxt-pokedex/components/Navigation.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav class="navigation container">
+    <div class="logo">
+      <a class="" href="#">
+        <img alt="Pokedex App" :src="require(`~/assets/images/pokedex-logo.png`)" width="120" />
+      </a>
+    </div>
+
+    <div class="nav-items">
+      <a class="button is-ghost">Home</a>
+      <a class="button is-ghost">About</a>
+    </div>
+  </nav>
+</template>
---
+++ pokedex-app/nuxt-pokedex/layouts/default.vue
@@ -0,0 +1,8 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div>
+    <Navigation />
+    <main class="container"><Nuxt /></main>
+    <Footer />
+  </div>
+</template>
