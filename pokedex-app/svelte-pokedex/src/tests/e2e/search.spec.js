be493730cadcaee8b42c45b17b46bc54869202ed
Paul Csapak <paul@base34.com>
2022-09-16 23:24:50 +0400

    svelte-pokedex: add playwright

---
+++ pokedex-app/svelte-pokedex/src/tests/e2e/search.spec.js
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test'
+
+const SEARCH_VALUE = 'pikachu';
+
+test('should be able to search/filter', async ({ page }) => {
+  // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
+  await page.goto('/')
+  // wait for 1 seconds, should be replaced by a more sophisticated async condition
+  await page.waitForTimeout(1000);
+  // Put search value into input
+  await page.locator('#search-input').fill(SEARCH_VALUE);
+  // Find the first tile element on the page and click it
+  await page.locator('div.tile').first().click();
+  // Expect page to be detail page
+  await expect(page).toHaveURL(/.*\/detail/);
+  // Expect page headline to contain same search value
+  await expect(page.locator('h3')).toContainText(SEARCH_VALUE)
+})
+
